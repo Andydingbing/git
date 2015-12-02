a3a38e8f1a267a191ea2af47a934f3686c86dca1
Andy <307841640@qq.com>
2015-12-02 21:59:22 +0800

    2.12.6程序

---
+++ C_Primer_Plus/2.12.6/2.12.6.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int printsmile();
+
+int main()
+{
+	printsmile();
+	printsmile();
+	printsmile();
+	printf("\n");
+
+	printsmile();
+	printsmile();
+	printf("\n");
+	
+	printsmile();
+	printf("\n");
+	
+	return 0;
+}
+
+int printsmile()
+{
+	printf("Smile!");
+	return 0;
+}
