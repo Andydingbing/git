99d1210a04f5c47d5a028ec4569cfd5907d4b62d
Andy <307841640@qq.com>
2015-12-02 21:58:56 +0800

    2.12.5程序

---
+++ C_Primer_Plus/2.12.5/2.12.5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	int toes = 10;
+	printf("toes = %d\n",toes);
+	printf("toes + toes = %d\n",toes + toes);
+	printf("toes * toes = %d\n",toes * toes);
+	return 0;
+}
