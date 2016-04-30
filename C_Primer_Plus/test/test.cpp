cc27370a5a187005b819af8ce3d3f8f74ae75494
丁彬 <307841640@qq.com>
2016-04-30 15:28:16 +0800

    测试了（a + b ++） * 6，说明b是在被a加之后才自增1的

--- C_Primer_Plus/test/test.cpp
+++ C_Primer_Plus/test/test.cpp
@@ -3,9 +3,15 @@
 
 int main()
 {
-	for (int i = 0;i < 10000; i ++) {
-		printf("%d\r",i);
+//	for (int i = 0;i < 10000; i ++) {
+//		printf("%d\r",i);
 //		fflush(stdout);
-		usleep(1000);
-	}
+//		usleep(1000);
+//	}
+	int a = 2;
+	int b = 3;
+	int c = (a + b++) * 6;
+	printf("c = %d\n",c);
+	printf("b = %d\n",b);
+	return 0;
 }
